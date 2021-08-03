b3897a296d1e7e16cb95816e1875c09232e2e1d1
Rishav <75078593+mairishavhoon@users.noreply.github.com>
2021-08-04 00:49:57 +0530

    created spam.py

---
+++ spam.py
@@ -0,0 +1,12 @@
+import pyautogui, time
+m = input("Input Spam Message : ")
+i = int(input("Enter spam amount :"))
+j = 1
+print("Spammer by Rishav started")
+time.sleep(15)
+while(j<i):
+    pyautogui.typewrite(m)
+    pyautogui.press("enter")
+    j = j+1
+
+print("Spammer by Rishav ended")    
